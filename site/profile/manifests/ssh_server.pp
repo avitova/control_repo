869dd8d1fc5d619d50da7a09578d47619a48e2ce
avitova <87140865+avitova@users.noreply.github.com>
2021-10-02 22:28:54 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -7,7 +7,7 @@ class profile::ssh_server {
                   enable => 'true',
         }
         ssh_authorized_key { 'root@master.puppet.vm'
-              ensure => present,
+              ensure => 'present',
               user => 'root',
               type => 'ssh-rsa',
               key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDeuC2bWKvtUhPcQZo4QHFTWNokOnDYje60+akKkrJwQthMlyUq1CkSYa1IG7jeG1rlrkEbU4NwnmzoxGmvAy2ESJNa3cMTAM1Eo/ygBdRBadZg8vwc+yI5lTBuPvS47cJeWPComn56N9dOGaVF31hPemDa0yrBjcFQKnliYEbhV9pwE4r7wLx3wFiwcB6vF21haYkRjQaR+tUp9DNTIcSwqQm1TiLBIn7y+335JNsQO/lp3PbUaCH/uIe5wvnguCEtyPhqm/8Og49EPJYR9m9dHJtxEp9zwPirBG54Xc54kcfZdlqJdo8qf0qwz7bBciUwNDnrnKzclDWNzWPgYuId',
